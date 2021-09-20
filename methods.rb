84c011d9336902f047e76a20e44698668dd5461a
jaenwawe <nwawe.jae@gmail.com>
2021-09-19 20:08:31 -0500

    commit tree 8f2bf40244ef1c2f7fd3310c559c417850ef3ffe

--- methods.rb
+++ methods.rb
@@ -1,1 +1,12 @@
 # Your code here!
+def my_method(param)
+    puts "Running myFunction"
+    param number + 1
+end
+
+
+
+greet_programmer = "Hello, programmer!"
+Failure/Error: expect { greet_programmer }.to output(a_string_including("Hello, programmer!")).to_stdout
+
+puts greet_programmer
